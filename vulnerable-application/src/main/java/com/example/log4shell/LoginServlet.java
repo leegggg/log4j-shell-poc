6422e3e83f3750ea7cb1d243795bee6d83d51060
ylin <yizhou.lin@outlook.com>
2022-03-18 13:51:17 +0800

    version

--- vulnerable-application/src/main/java/com/example/log4shell/LoginServlet.java
+++ vulnerable-application/src/main/java/com/example/log4shell/LoginServlet.java
@@ -9,13 +9,12 @@ import javax.servlet.annotation.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 @WebServlet(name = "loginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Logger logger = LogManager.getLogger(com.example.log4shell.log4j.class);  // vulnerable code
+        Logger logger = LogManager.getLogger(com.example.log4shell.log4j.class); // vulnerable code
         String userName = req.getParameter("uname");
         String password = req.getParameter("password");
 
@@ -23,12 +22,11 @@ public class LoginServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
         out.println("<html><body>");
 
-        logger.error(req.getHeader("User-Agent"));  // vulnerable code
-        if(userName.equals("admin") && password.equals("password")){
+        logger.error(req.getHeader("User-Agent")); // vulnerable code
+        if (userName.equals("admin") && password.equals("password")) {
             out.println("Welcome Back Admin");
-        }
-        else{
-            logger.error(userName);  // vulnerable code
+        } else {
+            logger.error(userName); // vulnerable code
             out.println("<code> the password you entered was invalid, <u> we will log your information </u> </code>");
         }
     }
